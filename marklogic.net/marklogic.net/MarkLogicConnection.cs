25377d029af51c513e99d1af1b77550f19008576
budziewski <budziewski@gmail.com>
2016-04-24 22:44:41 +0200

    a

--- marklogic.net/marklogic.net/MarkLogicConnection.cs
+++ marklogic.net/marklogic.net/MarkLogicConnection.cs
@@ -1,6 +1,6 @@
 ﻿namespace marklogic.net
 {
-    public class MarkLogicConnection
+    public class MarkLogicConnection : IMarkLogicConnection
     {
         public string Host { get; private set; }
         public string UserName { get; private set; }
@@ -22,5 +22,15 @@
         {
             return new MarkLogicSession(this);
         }
+    }
+
+    public interface IMarkLogicConnection
+    {
+        MarkLogicSession OpenSession();
+        string Host { get; }
+        string UserName { get; }
+        string Password { get; }
+        int Port { get; }
+        int Timeout { get; set; }
     }
 }
