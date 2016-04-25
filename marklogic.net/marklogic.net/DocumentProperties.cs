ce716b546507304bb272429349415ea9d8ef6dbb
ii@ limaK (CTO Monitoring) <kamil.budziewski@ihs.com>
2016-04-25 07:37:55 +0200

    defaults

--- marklogic.net/marklogic.net/DocumentProperties.cs
+++ marklogic.net/marklogic.net/DocumentProperties.cs
@@ -4,8 +4,14 @@ namespace marklogic.net
 {
     public class DocumentProperties
     {
+        public DocumentProperties()
+        {
+            Collections = new List<string>();
+            Permissions = new List<Permission>();
+        }
+
         public string DocumentUri { get; set; }
-        public List<Permission> Permissions{ get; set; }
+        public List<Permission> Permissions { get; set; }
         public List<string> Collections { get; set; }
     }
 }
