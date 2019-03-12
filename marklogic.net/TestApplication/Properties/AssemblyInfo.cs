e4a901d94dc85a5f59c74fe10171f2cd8bca8f87
slawomir.rzeppa <slawomir.rzeppa@ihsmarkit.com>
2019-03-12 09:00:26 +0100

    Delete AssemblyInfo.cs

--- marklogic.net/TestApplication/Properties/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Runtime.InteropServices;
-
-[assembly: Guid("9b9a226a-ee71-4c89-81c9-c20bf640805f")]
--- marklogic.net/marklogic.net/Properties/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Runtime.InteropServices;
-
-[assembly: Guid("02a4bc2b-99b1-4d93-b9eb-313c208b1386")]
